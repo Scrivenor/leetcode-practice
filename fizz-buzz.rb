bd1ff342f7ec405b47c5a91ef1915f2cb5f1c354
Scrivenor <scrivenor@protonmail.com>
2022-03-27 16:45:39 -0400

    Case/when didn't seem to work the way I expected it to. Ended up using if/elsif and it worked fine.

---
+++ fizz-buzz.rb
@@ -0,0 +1,28 @@
+# @param {Integer} n
+# @return {String[]}
+def fizz_buzz(n)
+  retArray = []
+  i = 0
+
+  while i <= n-1
+    
+    if ((i+1) % 3) == 0 && ((i+1) % 5) == 0
+      retArray[i] = "Fizzbuzz"
+    elsif ((i+1) % 3) == 0
+      retArray[i] = "Fizz"
+    elsif ((i+1) % 5) == 0
+      retArray[i] = "Buzz"
+    else
+      retArray[i] = (i+1).to_s
+    end
+    i += 1
+
+  #   when ((i+1) % 3) == 0 && ((i+1) % 5) == 0
+  #     retArray[i] = "FizzBuzz"
+  end
+
+  retArray
+
+end
+
+p fizz_buzz(150)
